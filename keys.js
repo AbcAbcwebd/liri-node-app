4fed1c6a207f9f1395855f53c8e712293de878eb
Owen Essen <abc_abcwebd@yahoo.com>
2017-07-21 09:35:54 -0400

    Liri now successfully interacts with Twitter API.

--- keys.js
+++ keys.js
@@ -4,5 +4,5 @@ exports.twitterKeys = {
   consumer_key: 'fU6txpiNdSaL6xxAwFJLtnc6I',
   consumer_secret: 'zQituDBYlcFnyQUVlELWn7u0Eapo4JfrVVpqk3GI6N4TRfx7Dz',
   access_token_key: '888063656323428352-LmQo2s6FtaZpiRQePgtviLCHqmeJDfj',
-  access_token_secret: 'xm8pFadAG3sbVOCnP4EpcRE5MXh0J0RW9poF3UjqHACI7',
+  access_token_secret: 'xm8pFadAG3sbVOCnP4EpcRE5MXh0J0RW9poF3UjqHACI7'
 }
--- liri.js
+++ liri.js
@@ -1,3 +1,35 @@
 var request = require('request');
 var twitterKeys = require('./keys.js');
-console.log(twitterKeys);
+var command = process.argv[2];
+var Twitter = require('twitter');
+
+/*
+// Interprets user's commands 
+switch(command) {
+    case "my-tweets":
+        code block
+        break;
+    case "spotify-this-song":
+        code block
+        break;
+    case "movie-this":
+        code block
+        break;
+    case "do-what-it-says":
+        code block
+        break;
+};
+*/
+var accessCodes = twitterKeys.twitterKeys;
+console.log(accessCodes)
+var t = new Twitter(accessCodes);
+var params = {
+q: "Mark",
+count: 1
+} // this is the param variable which will have key and value ,the key is the keyword which we are interested in searching and count is the count of it
+t.get('search/tweets', params,searchedData); // get is the 
+/*function to search the tweet which three paramaters 'search/tweets'
+,params and a callback function.*/
+function searchedData(err, data, response) {
+console.log(data);
+}
